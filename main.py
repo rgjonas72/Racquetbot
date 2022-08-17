2a559b6b45dd841c4a4965c52e085a0b556e7b3d
rgjonas72 <50970915+rgjonas72@users.noreply.github.com>
2022-08-17 17:49:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -242,7 +242,8 @@ async def get_stats2(discord_id):
     data = t2a(body=df.to_numpy().tolist(), style=PresetStyle.ascii_borderless)
     embed.add_field(name="\u200b", value=f"```\n{data}\n```")
     '''
-    embed.add_field(name="```" + df.to_string(index=False)[0] + "```", value=f"```\n" + df.to_string(index=False)[1:] + "\n```")
+    cols, data = df.to_string(index=False).split('\n', 1)
+    embed.add_field(name="```" + cols + "```", value=f"```\n" + data + "\n```")
     return embed
 
 
