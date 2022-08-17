a6079b14e2a18b04a9a6f15e9dbf9041b3f3234c
rgjonas72 <50970915+rgjonas72@users.noreply.github.com>
2022-08-16 22:35:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,147 +1,193 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@rgjonas72 
-rgjonas72
-/
-Racquetbot
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-Racquetbot
-/
-main.py
-in
-main
- 
-
-Spaces
-
-4
-
-No wrap
-1
 # Rank 1 role
-2
-​
-3
+
 # 1200 elo start, 500 for shitter
-4
-​
-5
+
 # log score, avg point differential, winrate vs player, point dif vs player
-6
-​
-7
+
 # rated, unrated queue
-8
-​
-9
+
 import discord
-10
 import os
-11
 import math
-12
 import mysql.connector
-13
-​
-14
+
 mydb = mysql.connector.connect(
-15
     host = "localhost",
-16
     user = "racquetbot",
-17
     password = "racquet",
-18
     database = "racquetbot"
-19
 )
-20
-​
-21
+
 mydb.autocommit = True
-22
 cursor = mydb.cursor()
-23
-​
-24
-​
-25
+
+
 async def Probability(rating1, rating2):
-26
     return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
-27
-​
-28
-​
-29
+
+
 # Function to calculate Elo rating
-30
 # K is a constant.
-31
 # d determines whether
-32
 # Player A wins or Player B.
-33
 async def EloRating(winner, loser, queue, K=5):
-34
     winner_db_string = await get_db_string(winner, queue)
-35
     loser_db_string = await get_db_string(loser, queue)
-36
     # Ra for winner, Rb for loser
-37
     ###Ra = db[winner_db_string][1]
-38
     ###Rb = db[loser_db_string][1]
-39
-​
-40
+
     # To calculate the Winning
-41
     # Probability of Player B
-42
     Pb = await Probability(Ra, Rb)
-43
-​
-44
+
     # To calculate the Winning
-45
     # Probability of Player A
-@rgjonas72
-Commit changes
-Commit summary
-Create main.py
-Optional extended description
-Add an optional extended description…
- Commit directly to the main branch.
- Create a new branch for this commit and start a pull request. Learn more about pull requests.
- 
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-You have no unread notifications
+    Pa = await Probability(Rb, Ra)
+
+    # Calculate new ratings
+    Ra_new = Ra + K * (1 - Pa)
+    Rb_new = Rb + K * (0 - Pb)
+
+    ###db[winner_db_string][1] = Ra_new
+    ###db[loser_db_string][1] = Rb_new
+
+    # Return value changes in ratings
+    return Ra_new - Ra, Rb_new - Rb
+
+
+# Initiate discord client
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+# Parameters: ID of winner and loser, and string of queue type
+# Queue type either 'Ranked' or 'Friendly'
+async def input_win(winner, loser, season):
+    await check_player_status(winner, season);
+    await check_player_status(loser, season);
+
+    winner_elo, loser_elo = await EloRating(winner, loser, season)
+    # Leaderboard update function here probably
+
+
+async def check_player_status(id, season):
+    cursor.execute('select discord_id from `' + season + '` where discord_id = %s', (id,))
+    if cursor.fetchone() is None:
+        await add_player(id, season)
+
+
+async def add_player(id, season):
+    name = await get_player_name(id)
+    cursor.execute('select * from HighTierPlayers where discord_id=%s', (id,))
+    elo = 500
+    if cursor.fetchone() is not None:
+        elo = 1200
+    cursor.execute('insert into `' + season + '` values (%s, %s, %s, 0, 0)', (name, id, elo))
+
+
+async def add_season(season):
+    print(season)
+    cursor.execute('insert into seasons (season_name, primary_ranked, primary_unranked) values (%s, 0, 0)', (season,))
+    cursor.execute('create table `' + season + '` (player_name varchar(50), discord_id int, elo int, wins int, losses int)')
+
+
+async def set_primary_season_ranked(season):
+    # End the current season
+    cursor.execute('update seasons set primary_ranked = 0, end_date = now() where primary_ranked = 1')
+    # Set primary ranked season to specified season
+    cursor.execute('update seasons set primary_ranked = 1, start_date = now() where season_name = %s', (season,))
+
+
+async def add_high_tier_player(id):
+    cursor.execute('select * from HighTierPlayers where discord_id=%s', (id,))
+    if cursor.fetchone() is not None:
+        return 'Already a high tier player.'
+    name = await get_player_name(id)
+    cursor.execute('insert into HighTierPlayers values (%s, %s)', (name, id,))
+    ### Give them +700 elo here in current season?
+    return 'Player added'
+
+
+async def get_player_name(id):
+    n = await client.fetch_user(str(id))
+    # , n.discriminator
+    return n.display_name
+
+
+async def get_current_ranked_season():
+    cursor.execute('select season_name from seasons where primary_ranked = 1')
+    season = cursor.fetchone()
+    return season[0]
+
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    game = discord.Game(".help | RacquetBot")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # Authorized users are Ryan and TJ
+    auth_user = False
+    if message.author.id == 196404822063316992 or message.author.id == 164281046039265281:
+        auth_user = True
+
+    msg = str(message.content)
+    try:
+        n = await client.fetch_user(str(message.author.id))
+        print(message.author.id, f"{n.display_name}#{n.discriminator}")
+    except:
+        print(message.author.id)
+
+    if message.content.lower().startswith('.ranked'):
+        mentions = message.mentions
+        if len(mentions) != 2:
+            await message.channel.send("Must mention two players.")
+            return
+        # Winner is first player mentioned, loser is second
+        winner, loser = mentions
+        current_season = await get_current_ranked_season()
+        await input_win(str(winner.id), str(loser.id), current_season)
+
+    if message.content.lower().startswith('.stats'):
+        mentions = message.mentions
+        if len(mentions) > 1:
+            await message.channel.send('Can only mention one player.')
+        elif len(mentions) == 1:
+            id = str(mentions[0].id)
+        else:
+            id = str(message.author.id)
+
+        ### Get stats function here
+
+
+    if message.content.lower().startswith('.addhightierplayer'):
+        if not auth_user:
+            await message.channel.send('Not allowed to use this command.')
+        mentions = message.mentions
+        if len(mentions) != 1:
+            await message.channel.send('Can only mention one player.')
+        result = await add_high_tier_player(str(mentions[0].id))
+        await message.channel.send(result)
+
+    if message.content.lower().startswith('.addseason'):
+        season = str(msg.split('.addseason', 1)[1]).strip()
+        await add_season(season)
+        await message.channel.send('Season added. Type .primaryranked or .primaryunranked followed by the season name to set it as the current season.')
+
+    if message.content.lower().startswith('.primaryranked'):
+        season = str(msg.split('.primaryranked', 1)[1]).strip()
+        await set_primary_season_ranked(season)
+        await message.channel.send(f'{season} set as current ranked season.')
+
+
+# keep_alive()
+# client.run(os.environ['TOKEN'])
+TOKEN = open("/repo/discord_token.txt", "r").read()
+client.run(TOKEN)
