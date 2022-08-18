090950e1f6808a956eebeafe24a0282e722aaa32
rgjonas72 <50970915+rgjonas72@users.noreply.github.com>
2022-08-18 13:31:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ async def get_stats(discord_id):
 
 
     x = df.to_string(index=False, justify="left", col_space=[3,12,4,3,3]).split('\n', 1)
-    cols, data = x.split('\n', 1)
+    cols, data = x.replace('\n', '\n  ').split('\n', 1)
 
     embed.add_field(name=f"{name} stats", value=f"```{cols}``` ```\n{data}```", inline=False)
     embed.add_field(name=f"---------------", value=f"----------------", inline=False)
@@ -254,7 +254,7 @@ async def get_ladder(season):
     embed = discord.Embed(color=0x70ac64)
 
     x = df.to_string(index=False, justify='left', col_space=[3,12,4,3,3])
-    cols, data = x.replace('\n', '\n ').split('\n', 1)
+    cols, data = x.replace('\n', '\n  ').split('\n', 1)
     embed.add_field(name=f"{season} Ladder", value=f"```{cols}``` ```\n{data}```", inline=False)
     embed.add_field(name=f"---------------", value=f"----------------", inline=False)
     embed.add_field(name=f"{season} Ladder", value=f"```{x}```", inline=False)
