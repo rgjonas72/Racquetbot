d91ac35da92c5749a8c41d48caafb689c9247b3c
rgjonas72 <50970915+rgjonas72@users.noreply.github.com>
2022-08-18 20:37:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -251,16 +251,18 @@ async def get_ladder(season):
     df.columns = ['Rank', 'Name', 'Elo', 'W', 'L']
     embed = discord.Embed(color=0x70ac64)
 
-    #cols, data = df.to_string(index=False, col_space=[3,12,4,3,3]).replace('\n', '\n ').split('\n', 1)
-    #embed.add_field(name=f"{season} Ladder", value=f"```{cols}``` ```\n{data}```", inline=False)
+
+    cols, data = df.to_string(index=False, col_space=[3,12,4,3,3]).replace('\n', '\n ').split('\n', 1)
+    embed.add_field(name=f"{season} Ladder", value=f"```{cols}``` ```\n{data}```", inline=False)
 
     cols = df.columns
     ar = df.to_numpy()
-    out = []
+    out = [[cols]]
     for row in ar:
         out.append("{: <5} {: <20} {: <4} {: <4} {: <4}".format(*row))
-    out_string = '\n'.join(out)
-    embed.add_field(name=f"test", value=f"```{cols}``` ```\n{out_string}```", inline=False)
+    header, data = '\n'.join(out).split('\n', 1)
+    
+    embed.add_field(name=f"test", value=f"```{header}``` ```\n{data}```", inline=False)
 
     return embed
 
