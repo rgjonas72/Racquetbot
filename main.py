b09618568174dff032fa51f463ae344bbc36e3b7
rgjonas72 <50970915+rgjonas72@users.noreply.github.com>
2022-08-19 20:08:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -401,8 +401,9 @@ async def get_stats(discord_id):
         del df_history
         return embed
 
-    nwins = df_history.loc[df_history['winner_id'] == discord_id]
+    nwins = len(df_history.loc[df_history['winner_id'] == discord_id].index)
     nlosses = ngames - nwins
+    print(ngames, nwins, nlosses)
 
     as_player1_sums = df_history.loc[df_history['player1_id'] == discord_id]['player1_score'].sum()
     as_player2_sums = df_history.loc[df_history['player2_id'] == discord_id]['player2_score'].sum()
